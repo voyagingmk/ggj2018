4e62158dd30f3f72c2ac433a33add464780a46cf
Wyman-PC <voyagingmk@gmail.com>
2018-01-27 02:15:22 +0800

    no message

---
+++ New Unity Project/Assets/addShadow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class addShadow : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        transform.GetComponent<SpriteRenderer>().GetComponent<Renderer>().receiveShadows = true;
+        transform.GetComponent<SpriteRenderer>().GetComponent<Renderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
