3bae962f96b9f511e87b1156f735b89c929c2fa0
Wyman-PC <voyagingmk@gmail.com>
2018-01-27 11:47:17 +0800

    no message

---
+++ New Unity Project/Assets/roleCtrl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class roleCtrl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Transform camTrans = GameObject.FindWithTag("MainCamera").transform;
+        transform.LookAt(camTrans);
+    }
+}
