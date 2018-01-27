fb014ba341bfd911b1a979d131eab7ace636f542
Wyman-PC <voyagingmk@gmail.com>
2018-01-27 13:29:09 +0800

    支持移动

---
+++ New Unity Project/Assets/scripts/cameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour {
+
+    public GameObject followObject = null;
+    public Vector3 offset = new Vector3(0, 20, -17);
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = followObject.transform.position + offset;
+
+    }
+}
--- New Unity Project/Assets/scripts/keyboardCtrl.cs
+++ New Unity Project/Assets/scripts/keyboardCtrl.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class keyboardCtrl : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+    float speed = 3.0f;   //移动速度
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        // 使用上下方向键或者W、S键来控制前进后退
+        float translationZ = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float translationX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        transform.Translate(translationX, 0, translationZ); 
+    }
 }
