a29b01fc4fdbb3f8040a0b1969d75765a7490f85
Wyman-PC <voyagingmk@gmail.com>
2018-01-29 19:53:15 +0800

    no message

---
+++ New Unity Project/Assets/scripts/blink.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class blink : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Text t = GetComponent<Text>();
+        Sequence sequence = DOTween.Sequence();
+        sequence.Append(t.DOColor(new Color(0, 0, 0, 0), 0.5f));
+        sequence.Append(t.DOColor(new Color(0, 0, 0, 1), 0.5f));
+        sequence.SetLoops(-1);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
